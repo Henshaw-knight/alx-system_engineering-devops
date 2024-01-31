c110b55f5860109f1d525daf0edbe70d9b5c0a6e
Henshaw <josephhensh2000@gmail.com>
2024-01-31 10:39:36 -0800

    Use puppet make changes to configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Set up SSH configuration using Puppet
+file_line { 'find and use private key':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/holberton',
+  ensure => 'present'
+}
+
+file_line { 'disable password login':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
+  ensure => 'present'
+}
