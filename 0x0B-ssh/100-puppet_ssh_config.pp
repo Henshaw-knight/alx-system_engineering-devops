99dfe357133260b0be75ee73070ad1fd21b38160
Henshaw <josephhensh2000@gmail.com>
2024-01-31 10:43:49 -0800

    Use puppet to make changes to configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Set up SSH configuration using Puppet
 file_line { 'find and use private key':
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentifyFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
   ensure => 'present'
 }
 
