41f051ae9c6fc52176b7bec27623188869edbf4d
Henshaw <josephhensh2000@gmail.com>
2024-03-01 07:09:16 -0800

    Update script

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = argv[1]
 
-    response = requests.get(f"{base_url}/users/{employee_id}")
+    response = requests.get("{}//users/{}".format(
+        base_url, employee_id))
     employee_name = response.json().get("name")
 
-    response = requests.get(f"{base_url}/users/{employee_id}/todos")
+    response = requests.get("{}/users/{}/todos".format(
+        base_url, employee_id))
     todos = response.json()
     total_tasks = len(todos)
     no_of_done_tasks = 0
@@ -23,7 +25,7 @@ if __name__ == "__main__":
             no_of_done_tasks += 1
             done_tasks.append(data.get("title"))
 
-    print(f"Employee {employee_name} is done with \
-tasks({no_of_done_tasks}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, no_of_done_tasks, total_tasks))
     for title in done_tasks:
-        print(f"\t {title}")
+        print("\t {}".format(title))
