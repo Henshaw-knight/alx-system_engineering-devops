9d69dbde29de6e56014a5b89ee94db7833d1e89d
Henshaw <josephhensh2000@gmail.com>
2024-03-08 07:42:26 -0800

    Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,8 +10,12 @@ def top_ten(subreddit):
     headers = {
             'User-Agent': 'Mozilla/5.0'
     }
+    params = {
+            'limit': 10
+    }
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
@@ -23,5 +27,5 @@ def top_ten(subreddit):
         print(None)
     else:
         subreddit_children = reddit_data.get("data").get("children")
-        for i in range(10):
-            print(subreddit_children[i].get("data").get("title"))
+        for child in subreddit_children:
+            print(child.get("data").get("title"))
