4ad9827a2de7ed01e3d061bd032875ed883ae02f
Henshaw <josephhensh2000@gmail.com>
2024-03-08 07:30:47 -0800

    update file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,11 +12,16 @@ def top_ten(subreddit):
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
     reddit_data = response.json()
 
-    try:
+    if "data" not in reddit_data:
+        print(None)
+    else:
         subreddit_children = reddit_data.get("data").get("children")
         for i in range(10):
             print(subreddit_children[i].get("data").get("title"))
-    except Exception:
-        print(None)
