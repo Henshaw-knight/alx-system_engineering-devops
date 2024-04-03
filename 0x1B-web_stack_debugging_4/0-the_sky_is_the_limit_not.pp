472e3a59e463656c6dedea9b469aa7d8e88819e3
Henshaw <josephhensh2000@gmail.com>
2024-04-03 12:51:54 -0700

    Update file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,11 @@
 # Fix the nginx requests limit using puppet file
 exec { 'update_ulimit':
   command  => 'sed -i "s/15/4096" /etc/default/nginx',
-  provider => shell
+  provider => shell,
 }
 
 exec { 'restart_nginx':
   command  => 'service nginx restart',
   provider => shell,
-  require  => Exec['update_ulimit']
+  require  => Exec['updateulimit'],
 }
